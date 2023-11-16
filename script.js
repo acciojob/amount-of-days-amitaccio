fdd6a98e1fb31d74cb3c15ee6fa3e49a0cc723ee
amitaccio <142372667+amitaccio@users.noreply.github.com>
2023-11-16 22:39:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+/// century year must be divisible by 400.
+// and normal year must divisible by 4. 
+// noth known as leap year
+
+ let typeofyear = function daysOfAYear(year){
+	if(year%4==0 || year%400 == 0){
+		 return 366;
+	}
+	 else{
+		  return 365;
+	 }
+}
+  let y = prompt("Enter the year")
+ daysOfAYear(y);
+
+ 
